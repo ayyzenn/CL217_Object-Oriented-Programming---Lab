ff30fd9f7df5a2cbf38024af3879718da6710dd1
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-01-20 12:14:01 +0500

    dinner

---
+++ Assignments/1/Codes/Conditional Statements/if else/Q1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long x;
+    cout<<"Enter the number: ";
+    cin>>x;
+    if (x>0)
+    {
+        cout<<x<<" is a positive number."<<endl;
+    }
+    else
+    {
+        cout<<x<<" is a negative number."<<endl;
+    }
+    return 0;
+    
+}
