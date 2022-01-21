d6c71a8c4c1fab5134d9dc9d6ba8234f4378ee46
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-01-21 13:17:05 +0500

    kldfj;fasd

---
+++ Assignments/1/Codes/Conditional Statements/if else/Q2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long x;
+    cout<<"Enter the number: ";
+    cin>>x;
+    if (x%2==0)
+    {
+        cout<<x<<" is a even number."<<endl;
+    }
+    else
+    {
+        cout<<x<<" is a odd number."<<endl;
+    }
+    return 0;
+    
+}
