bd8f2214633e8d1611d6f729300c121582eab673
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-02-03 07:56:24 +0500

    fdgfdfdgfd

---
+++ Assignments/1/Codes/Loop/while loop/Q2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=0, j=0;
+    cout<<"Even numbers"<<endl;
+    while (i<=10)
+    {
+       if (i%2==0)
+        {
+            cout<<i<<endl;
+        }
+        i++;
+        
+    }
+    cout<<endl;
+    cout<<"Odd numbers"<<endl;
+    while (j<=10)
+    {
+        if (j%2 != 0)
+        {
+            cout<<j<<endl;
+        }
+        j++;
+    }
+    
+}
+
+
+
+
+
+
