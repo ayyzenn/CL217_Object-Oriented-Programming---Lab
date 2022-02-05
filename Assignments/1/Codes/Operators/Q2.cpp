517f34c25db77dcc74dd56aa9a6938cdddb0a302
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-02-05 07:36:02 +0500

    a;sklfja;kf

---
+++ Assignments/1/Codes/Operators/Q2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float rupees, doller;
+    cout<<"Enter the amount in rupees: ";
+    cin>>rupees;
+    doller=rupees*0.0064;              // The value I use for one doller is 157.05 at my time.
+    cout<<"The amount in dollers is: "<<doller<<endl;
+    return 0;
+}
+
+
+
+
+
+
