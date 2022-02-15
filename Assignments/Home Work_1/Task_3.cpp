6245f244ef725cd86fecdcfd930bb3b9cbe60616
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-02-15 18:16:05 +0500

    s;jfklsjf;as

---
+++ Assignments/Home Work_1/Task_3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using  namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the number: ";
+    cin>>a;
+    if (a%2==0)
+    {
+        cout<<"Number is Even."<<endl;
+    }
+    else
+    {
+        cout<<"Number is Odd."<<endl;
+    }
+}
