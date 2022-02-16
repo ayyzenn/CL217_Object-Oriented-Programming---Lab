662154fcd897aa2c768482a300c2d7dc7201d18a
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-02-16 15:22:39 +0500

    skfja;lskfj

---
+++ Assignments/Home Work_2/Task1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float doller,rupees;
+    cout<<"Enter the amount in dollers: ";
+    cin>>doller;
+    rupees=doller*157.05;               // The value I use for one doller is 157.05 at my time
+    cout<<"The amount in rupees is: "<<rupees<<endl;
+    return 0;
+}
