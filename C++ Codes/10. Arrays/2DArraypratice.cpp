26ecb4105ff6f020a6dad16a7c48afee3c14f1b5
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-03-20 06:24:57 +0500

    asjkfkla

---
+++ C++ Codes/10. Arrays/2DArraypratice.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+ 
+int main () {
+   // an array with 5 rows and 2 columns.
+   int a[5][2] = { 
+
+       {0,0}, 
+       {1,2}, 
+       {2,4}, 
+       {3,6},
+       {4,8}
+       
+       };
+
+
+       cout<<a[3][1];
+ 
+
+
+ string name="ali";
+
+ switch (name)
+ {
+ case 'ali':
+     break;
+ 
+ default:
+     break;
+ }
+
+ 
+   return 0;
+}
