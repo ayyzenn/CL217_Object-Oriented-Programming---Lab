0d85aab6bb63140c2f53d9883fb7664f3d50e46b
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-03-21 11:34:38 +0500

    asdklfj;lakf

---
+++ C++ Codes/10. Arrays/accessingArray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+int myNum[] ={1,2,3,4,5};
+
+
+cout<<myNum[0]<<endl;
+cout<<myNum[1]<<endl;
+cout<<myNum[2]<<endl;
+cout<<myNum[3]<<endl;
+cout<<myNum[4]<<endl;
+
+  return 0;
+} 
