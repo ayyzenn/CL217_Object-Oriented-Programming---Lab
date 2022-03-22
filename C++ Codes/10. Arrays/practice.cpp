44ccd1c3a209f7e82f5fbb05246a95e11e0eb6b6
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-03-22 11:27:09 +0500

    jklasdjfl;asjf

---
+++ C++ Codes/10. Arrays/practice.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+ 
+int main () {
+
+
+int myNum[4] ={1,2,3,888};
+
+int a[2][2]=  { {1,3},
+                {4,5}
+
+                  };
+
+cout<<a[0][1];
+
+
+int a=cout<<a[0][1];
+
+// string name= "Ali";
+// for(char n : name)
+// {
+//   cout<<n<<endl;
+// }
+
+
+// for (int i = 0; i<sizeof(myNum)/sizeof(int); i++)
+// {
+//   cout<<myNum[i]<<endl;
+// }
+
+// for(int number : myNum)
+// {
+//   cout<<number<<endl;
+// }
+
+
+
+//cout<<sizeof(int);
+
+//cout<<sizeof(myNum)/sizeof(int);
+
+//cout<<myNum[2];
+
+
+
+
+
+  // int myNum[] ={1,2,3,4,5,6,7,8};
+
+  // cout<<sizeof(myNum)/sizeof(int)<<endl;
+
+//   string num[] ={"1","2","3","4"};
+
+//   cout<<sizeof(num)/sizeof(string);
+
+
+// for(int i = 0; i <sizeof(myNum)/sizeof(int); i++)
+// {
+
+//   cout<<sizeof(myNum)/sizeof(int);
+  
+// }
+  
+ 
+   return 0;
+}
