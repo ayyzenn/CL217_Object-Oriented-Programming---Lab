69453c69cd0dab1514c4d3afcec9d682ef85d87a
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-03-22 11:26:40 +0500

    asdjkflas;klfjs

---
+++ C++ Codes/10. Arrays/empthmemberArray.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+int myNum[6] ={1,2,3};
+
+string names[4] = {"Ali", "Asia", "Zain", "Zainab"};
+for(int i = 0; i < 4; i++) {
+  cout << names[i] << "\n";
+}
+
+  return 0;
+}  
