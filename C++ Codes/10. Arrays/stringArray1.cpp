87004e2fa5f1c34a4ce1944c100847fbdda49710
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-03-23 18:02:52 +0500

    sjdlkjas;d

---
+++ C++ Codes/10. Arrays/stringArray1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+int myNum[6] ={1,2,3};
+
+string names[4] = {"Ali", "Asia", "Zain", "Zainab"};
+for(int i = 0; i < 4; i++) {
+cout << i << ": " << names[i] << "\n";
+
+}
+
+  return 0;
+}  
