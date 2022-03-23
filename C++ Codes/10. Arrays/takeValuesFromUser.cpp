b1b9b29eba1d1b122e60777af90ff2e7410406f6
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-03-23 18:03:20 +0500

    sdklfja;lsj

---
+++ C++ Codes/10. Arrays/takeValuesFromUser.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int numbers[5];
+
+cout<<"Enter 5 numbers: "<<endl;
+
+//  store input from user to array
+
+for(int i =0 ; i<5; i++)
+{
+    cin>>numbers[i];
+}
+
+cout<<"The numbers are: ";
+
+for (int j=0; j<5 ; j++)
+{
+    cout<<numbers[j]<<" ";
+}
+
+return 0;
+
+}
