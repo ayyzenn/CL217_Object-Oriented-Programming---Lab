6fdde926e7511fff15a4b0512b7d3f2f2177bba4
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-04-04 09:00:24 +0500

    a;kdlafj;ak

---
+++ C++ Codes/15. File Handling in C++/readingtillend1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+	string ch;
+	fstream st; // step 1: Creating object of ifstream class
+	st.open("abc.txt",ios::in);   // Step 2: open a file
+    
+	while(!st.eof())
+	{
+	getline(st,ch);  // Step 4: Reading from a file
+	//st>>ch;
+		cout<<ch<<endl; //Print on console
+	}
+	st.close(); // Step 5: Closing file
+
+	system("pause");
+    return 0;
+}
