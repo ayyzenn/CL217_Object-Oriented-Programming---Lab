e25a7f2fa20d705571e424b4f39f9b60a259aaf9
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-04-07 04:07:02 +0500

    klajfkls;f

---
+++ C++ Codes/16. Classes and Objects in C++/classandobjectssyntax.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+#include<iostream>
+
+class Date
+{
+   // Access specifier
+    private:
+
+    public:
+   
+}; // end of class body
+
+
+ 
+int main () {
+
+    // Declare an object of class 
+    Date date;
+
+   
+   return 0;
+}
