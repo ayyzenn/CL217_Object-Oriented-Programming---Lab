d9753865cffcfc0874f0456a5ad562cda40c3bd7
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-04-09 09:59:33 +0500

    as;klfja;sf

---
+++ C++ Codes/16. Classes and Objects in C++/ConstructorSyntax.cpp
@@ -0,0 +1,45 @@
+using namespace std;
+#include<iostream>
+
+class Student
+{
+int a=0;
+float b=0.0;
+
+public:
+Student()   // explicit defualt constructor
+{
+          // int 0   float 0.0  string null
+}
+
+Student(int a, float b)   // explicit defualt constructor
+{
+
+}
+
+int add(int i, int j)
+{
+return 0;
+}
+
+
+}; //end of class
+
+int main () {
+
+   Student s;
+ 
+   return 0;
+}
+
+// object is the result (intance) of class
+// object instantiation
+// getters() and setters()
+// defining functions outside the clas ::
+
+// explicit default constructor
+//implicit =======
+
+
+
+
