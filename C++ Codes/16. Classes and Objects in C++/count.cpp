1fa7ca794c33cee46f6c520faa6928618f39b6b2
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-04-11 17:41:22 +0500

    ajfsldfj;asjf

---
+++ C++ Codes/16. Classes and Objects in C++/count.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+////////////////////////////////////////////////////////////////
+class Count
+{
+private:
+int a;
+
+
+
+void add();
+
+
+};
+
+
+class Count{
+
+};
+
+int main()
+{
+
+    Count c1;
+    c1.a=50;
+
+return 0;
+}
