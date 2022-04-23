97ec61e036a5869476c39357b3217ca0a9007172
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-04-23 09:26:24 +0500

    a;lkdfjal;ksfjskld

---
+++ C++ Codes/16. Classes and Objects in C++/test1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  char name[4];
+  int n;
+
+
+  for (int i =0;i< 5;i++){
+  cout << "ENTER THE NAME ";
+  cin.get(name, 4);
+
+  cout << "ENTER THE NUMBER ";
+  cin >> n;
+  }
+  return 0;
+
+}
