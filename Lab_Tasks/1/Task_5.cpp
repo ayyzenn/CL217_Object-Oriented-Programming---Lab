d86bb7a3908332ad47559661faa8414d8d0b2e76
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-05-06 13:27:50 +0500

    askdjfalksfjwo

---
+++ Lab_Tasks/1/Task_5.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int salary;
+
+    cout << "Enter your salary: ";
+    cin >> salary;
+
+    if ( salary < 10000 ) 
+    {
+        cout << "Your Salary is: " << salary << endl;
+        cout << "And no fund deducted." << endl;
+
+    }
+
+    else
+
+    {
+
+        if ( salary >= 10000 && salary < 20000 )
+
+        {
+
+            int fund_1 =1000;
+            salary-=fund_1;
+
+            cout << "Net payable amount is: " << salary << " \nAnd fund deducted is: " << fund_1 <<endl;
+
+        }
+
+        else if ( salary >= 20000 )
+
+        {
+
+            float fund_2 = 0.07;
+            float fund_3 = salary * fund_2;
+            salary -= fund_3;
+
+            cout << "Net payable amount is: " << salary << " \nAnd fund deducted is: " << fund_3 <<endl;
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
