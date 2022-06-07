ac244b4cbd72b9b04fd5c47dc2d008d9f4104812
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-06-07 13:55:53 +0500

    skdfjklsjf;lafl

---
+++ OOP_Lab_Work/OOP_1/l7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string fname,lname,fullname;
+    cin>>fname;
+    cin>>lname;
+    fullname=fname+" "+lname;
+    cout<<fullname<<endl;
+    return 0;
+}
