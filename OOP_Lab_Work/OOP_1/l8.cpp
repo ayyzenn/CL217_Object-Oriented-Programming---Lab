aeb4e3f14f921523954f076f4018041395663351
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-06-07 13:56:43 +0500

    skdfjklasj;f

---
+++ OOP_Lab_Work/OOP_1/l8.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string test;
+    cout<<"Enter a string witout spaces: ";
+    cin>>test;
+    cout<<test.length()<<endl;
+    return 0;
+}
