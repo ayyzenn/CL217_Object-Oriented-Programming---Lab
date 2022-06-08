9928f1f504568ec9c9681fb4d3265f45df20a2f3
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-06-08 10:39:14 +0500

    dskfsdklfsdk

---
+++ OOP_Lab_Work/OOP_1/l9_2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string a="Saad";
+    a[0]='s';   // single '' is used "" is not allowed.
+    cout<<a<<endl;
+    return 0;
+}
