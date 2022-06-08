ef28dff9d6028f94a1bf16d84b127d9376e710e8
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-06-08 10:39:32 +0500

    jsdkfjklsjfa;lsf

---
+++ OOP_Lab_Work/OOP_1/l10.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 1;
+    switch (a)
+    {
+        default:
+        cout << "DEF"<<endl;
+        case 1:
+        cout <<1<<endl;
+        case 0:
+        cout <<0<<endl;
+    }
+    return 0;
+}
