70b80d1e5d31233ca831d59187279a8ba00e1184
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-06-26 10:08:46 +0500

    ksdjflasj;fslkfdj;

---
+++ OOP_Lab_Work/Rough work/2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+class Rectangle
+{
+    private :
+        int length;
+        int width;
+    public:
+        void setData(int len, int wid)
+        {
+            length = len;
+            width = wid;
+        }
+        int getArea()
+        {
+            return length * width ;
+        }
+    friend double getCost(Rectangle);//friend of class Rectangle
+};
+// end of class body
+//friend function getCost can access private member of class
+double getCost (Rectangle rect)
+{
+    double cost;
+    cost = rect.length * rect.width * 5;
+    return cost;
+}
+
+int main ()
+{
+    Rectangle floor;
+
+    floor.setData(20,3);
+    
+    cout<<"Area is:"<<floor.getArea()<<endl;
+    cout << "Expense " << getCost(floor) << endl;
+    return 0;
+}
